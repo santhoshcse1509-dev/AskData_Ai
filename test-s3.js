577f0ab5e31df502670e2555a6a6037858b6f4c4
shrinidhi-1609 <mshrinidhi06@gmail.com>
2026-06-27 17:25:06 +0530

    remove test file with credentials

--- test-s3.js
+++
@@ -1,3 +0,0 @@
-const { S3Client, ListBucketsCommand } = require('@aws-sdk/client-s3');
-const s3 = new S3Client({ region: 'ap-south-1', credentials: { accessKeyId: 'AKIAWMM3Y3SXBRVCHDPU', secretAccessKey: '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' }});
-s3.send(new ListBucketsCommand({})).then(r => console.log('SUCCESS:', JSON.stringify(r.Buckets))).catch(e => console.log('ERROR:', e.message));
